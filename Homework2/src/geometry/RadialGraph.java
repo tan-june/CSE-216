6f52c9443c98a1840e22b693cc2f9deb7806c751
Tanmay Singhal <43653881+tan-june@users.noreply.github.com>
2023-04-06 14:17:15 -0400

    Square+Radial Graph Testing Completed

--- Homework2/src/geometry/RadialGraph.java
+++ Homework2/src/geometry/RadialGraph.java
@@ -49,33 +49,45 @@ public class RadialGraph extends Shape {
         return this.center;
     }
 
-    @Override
+   @Override
     public String toString() {
-        List<Point> PointSort = new ArrayList<>(neighbors);
-        PointSort.sort((p1, p2) -> {
-            double angle1 = Math.atan2(-p1.getY(), -p1.getX());
-            double angle2 = Math.atan2(-p2.getY(), -p2.getX());
+        // Translate the shape to have its center at (0,0)
+        double centerX = center().getX();
+        double centerY = center().getY();
+        translateBy(-centerX, -centerY);
+
+        // Sort the points by angle with respect to the positive x-axis
+        List<Point> points = neighbors;
+        Point center = center();
+        Collections.sort(points, (p1, p2) -> {
+            double angle1 = Math.toDegrees(Math.atan2(p1.getY() - centerY, p1.getX() - centerX));
+            double angle2 = Math.toDegrees(Math.atan2(p2.getY() - centerY, p2.getX() - centerX));
+            if (angle1 < 0) {
+                angle1 += 360;
+            }
+            if (angle2 < 0) {
+                angle2 += 360;
+            }
             return Double.compare(angle1, angle2);
         });
+
+        // Build the string representation of the shape
         StringBuilder sb = new StringBuilder("[");
         sb.append(center.toString());
-
-        for (Point point : PointSort) {
-            sb.append("; ");
-            sb.append("(");
-            sb.append(point.name);
-            sb.append(", ");
-            sb.append(point.getX());
-            sb.append(", ");
-            sb.append(point.getY());
-            sb.append(")");
+        for (Point p : points) {
+            sb.append("; ").append(p.toString());
         }
         sb.append("]");
+
+        // Translate the shape back to its original position
+        translateBy(centerX, centerY);
 
         return sb.toString();
     }
 
+
     public static void main(String... args) {
+
         Point center = new Point("center", 0, 0);
         Point east = new Point("east", 1, 0);
         Point west = new Point("west", -1, 0);
@@ -84,37 +96,100 @@ public class RadialGraph extends Shape {
         Point toofarsouth = new Point("south", 0, -2);
 
         System.out.println("Single Point");
-        // A single node is a valid radial graph.
         RadialGraph lonely = new RadialGraph(center);
-        // Must print: [(center, 0.0, 0.0)]
         System.out.println(lonely);
+        //Single Point
+        //[(center, 0.0, 0.0)]
 
+        //Rotating Test
         System.out.println("\n" + "Rotate Test");
         Shape g = new RadialGraph(center, Arrays.asList(north, south, east, west));
-        // [(center, 0.0, 0.0); (east, 1.0, 0.0); (north, 0.0, 1.0); (west, -1.0, 0.0); (south, 0.0, -1.0)]
-        System.out.println(g);
-        // After this counterclockwise rotation by 90 degrees, "north" must be at (-1, 0), and similarly for all the
-        g = g.rotateBy(90);
         System.out.println(g);
         g = g.rotateBy(90);
         System.out.println(g);
-        g = g.rotateBy(90);
+        g = g.rotateBy(180);
         System.out.println(g);
-        g = g.rotateBy(90);
+        g = g.rotateBy(270);
         System.out.println(g);
+        g = g.rotateBy(180);
+        System.out.println(g);
+        System.out.println();
+        //Rotate Test
+        //[(center, 0.0, 0.0); (east, 1.0, 0.0); (north, 0.0, 1.0); (west, -1.0, 0.0); (south, 0.0, -1.0)]
+        //[(center, 0.0, 0.0); (south, 1.0, 0.0); (east, 0.0, 1.0); (north, -1.0, 0.0); (west, 0.0, -1.0)]
+        //[(center, 0.0, 0.0); (north, 1.0, 0.0); (west, 0.0, 1.0); (south, -1.0, 0.0); (east, 0.0, -1.0)]
+        //[(center, 0.0, 0.0); (west, 1.0, 0.0); (south, 0.0, 1.0); (east, -1.0, 0.0); (north, 0.0, -1.0)]
+        //[(center, 0.0, 0.0); (east, 1.0, 0.0); (north, 0.0, 1.0); (west, -1.0, 0.0); (south, 0.0, -1.0)]
 
+        //Translation Test
         System.out.println("\n" + "Translation Test");
         //Translate Tests
         Shape t = new RadialGraph(center, Arrays.asList(north, south, east, west));
         System.out.println(t);
-        t = t.translateBy(10,10);
+        t = t.translateBy(100,100);
         System.out.println(t);
         t = t.translateBy(-10,-10);
         System.out.println(t);
+        t = t.translateBy(-90,-90);
+        System.out.println(t);
+        System.out.println();
+        //[(center, 0.0, 0.0); (east, 1.0, 0.0); (north, 0.0, 1.0); (west, -1.0, 0.0); (south, 0.0, -1.0)]
+        //[(center, 100.0, 100.0); (east, 101.0, 100.0); (north, 100.0, 101.0); (west, 99.0, 100.0); (south, 100.0, 99.0)]
+        //[(center, 90.0, 90.0); (east, 91.0, 90.0); (north, 90.0, 91.0); (west, 89.0, 90.0); (south, 90.0, 89.0)]
+        //[(center, 0.0, 0.0); (east, 1.0, 0.0); (north, 0.0, 1.0); (west, -1.0, 0.0); (south, 0.0, -1.0)]
+
+
+
+        //Break Your Code Test
+        System.out.println("Break Your Code Test");
+        Point center1 = new Point("center", 10,14);
+        Point east1 = new Point("east", 11,14);
+        Point west1 = new Point("west", 9,14);
+        Point north1 = new Point("north", 10,15);
+        Point south1 = new Point("south", 10,13);
+        Shape test1 = new RadialGraph(center1, Arrays.asList(north1, south1, east1, west1));
+        System.out.println("Original:    " + test1);
+        test1 = test1.translateBy(100,100);
+        System.out.println("Translate:   " + test1);
+        test1 = test1.translateBy(-10,-10);
+        System.out.println("Translate:   " + test1);
+        test1 = test1.rotateBy(90);
+        System.out.println("Rotate   :   " + test1);
+        test1 = test1.rotateBy(90);
+        System.out.println("Rotate   :   " + test1);
+        test1 = test1.rotateBy(90);
+        System.out.println("Rotate   :   " + test1);
+        test1 = test1.rotateBy(90);
+        System.out.println("Rotate   :   " + test1);
+        test1 = test1.rotateBy(-90);
+        System.out.println("Rotate   :   " + test1);
+        test1 = test1.rotateBy(-90);
+        System.out.println("Rotate   :   " + test1);
+        test1 = test1.rotateBy(-90);
+        System.out.println("Rotate   :   " + test1);
+        test1 = test1.rotateBy(-90);
+        System.out.println("Rotate   :   " + test1);
+        System.out.println();
+        //Break Your Code Test
+        //Original:    [(center, 10.0, 14.0); (east, 11.0, 14.0); (north, 10.0, 15.0); (west, 9.0, 14.0); (south, 10.0, 13.0)]
+        //Translate:   [(center, 110.0, 114.0); (east, 111.0, 114.0); (north, 110.0, 115.0); (west, 109.0, 114.0); (south, 110.0, 113.0)]
+        //Translate:   [(center, 100.0, 104.0); (east, 101.0, 104.0); (north, 100.0, 105.0); (west, 99.0, 104.0); (south, 100.0, 103.0)]
+        //Rotate   :   [(center, 100.0, 104.0); (south, 101.0, 104.0); (east, 100.0, 105.0); (north, 99.0, 104.0); (west, 100.0, 103.0)]
+        //Rotate   :   [(center, 100.0, 104.0); (west, 101.0, 104.0); (south, 100.0, 105.0); (east, 99.0, 104.0); (north, 100.0, 103.0)]
+        //Rotate   :   [(center, 100.0, 104.0); (north, 101.0, 104.0); (west, 100.0, 105.0); (south, 99.0, 104.0); (east, 100.0, 103.0)]
+        //Rotate   :   [(center, 100.0, 104.0); (east, 101.0, 104.0); (north, 100.0, 105.0); (west, 99.0, 104.0); (south, 100.0, 103.0)]
+        //Rotate   :   [(center, 100.0, 104.0); (north, 101.0, 104.0); (west, 100.0, 105.0); (south, 99.0, 104.0); (east, 100.0, 103.0)]
+        //Rotate   :   [(center, 100.0, 104.0); (west, 101.0, 104.0); (south, 100.0, 105.0); (east, 99.0, 104.0); (north, 100.0, 103.0)]
+        //Rotate   :   [(center, 100.0, 104.0); (south, 101.0, 104.0); (east, 100.0, 105.0); (north, 99.0, 104.0); (west, 100.0, 103.0)]
+        //Rotate   :   [(center, 100.0, 104.0); (east, 101.0, 104.0); (north, 100.0, 105.0); (west, 99.0, 104.0); (south, 100.0, 103.0)]
 
         System.out.println("\n" + "Illegal Argument Exceptions");
         // This line must throw IllegalArgumentException, since the edges will not be of the same length
         RadialGraph nope = new RadialGraph(center, Arrays.asList(north, toofarsouth, east, west));
         System.out.println(nope);
+        //Illegal Argument Exceptions
+        //Exception in thread "main" java.lang.IllegalArgumentException: Whatever error you put!
+        //	at geometry.RadialGraph.<init>(RadialGraph.java:17)
+        //	at geometry.RadialGraph.main(RadialGraph.java:196)
     }
 }
--- Homework2/src/geometry/Square.java
+++ Homework2/src/geometry/Square.java
@@ -2,6 +2,7 @@ package geometry;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Square extends Shape {
@@ -75,12 +76,21 @@ public class Square extends Shape {
 
     @Override
     public String toString() {
-        List<Point> PointSort = new ArrayList<>(points);
-        PointSort.sort((p1, p2) -> {
-            double angle1 = Math.atan2(-p1.getY(), -p1.getX());
-            double angle2 = Math.atan2(-p2.getY(), -p2.getX());
-            int compare = Double.compare(angle1, angle2);
-            return compare;
+        double centerX = center().getX();
+        double centerY = center().getY();
+        translateBy(-centerX, -centerY);
+        List<Point> PointSort = points;
+        Point center = center();
+        Collections.sort(PointSort, (p1, p2) -> {
+            double angle1 = Math.toDegrees(Math.atan2(p1.getY() - centerY, p1.getX() - centerX));
+            double angle2 = Math.toDegrees(Math.atan2(p2.getY() - centerY, p2.getX() - centerX));
+            if (angle1 < 0) {
+                angle1 += 360;
+            }
+            if (angle2 < 0) {
+                angle2 += 360;
+            }
+            return Double.compare(angle1, angle2);
         });
         StringBuilder sb = new StringBuilder("[");
         for (int i = 0; i < PointSort.size(); i++) {
@@ -106,33 +116,122 @@ public class Square extends Shape {
         Point north = new Point("C", -10, -10);
         Point south = new Point("D", 10, -10);
         Point toofarsouth = new Point("south", 0, -2);
-        //east -> north -> west -> south
 
         System.out.println("Square Testing");
-
-        //Print Square
+        //Print Square @ Origin -> ABCD
         Shape lonely = new Square(east, west, north, south);
         System.out.println(lonely);
         System.out.println(lonely.center());
+        System.out.println();
+        //Square Testing
+        //[(A, 10.0, 10.0); (B, -10.0, 10.0); (C, -10.0, -10.0); (D, 10.0, -10.0)]
+        //(Center of Square, 0.0, 0.0)
 
-        System.out.println("\n" + "Translation Tests");
-        System.out.println("Original: " + lonely);
-        lonely = lonely.translateBy(10,10);
-        System.out.println("          " + lonely);
-        lonely = lonely.translateBy(-10,-10);
-        System.out.println("          " + lonely);
+        lonely = lonely.translateBy(100,100);
+        System.out.println("Print Square Not at Origin");
+        System.out.println(lonely);
+        System.out.println(lonely.center());
+        System.out.println();
+        lonely = lonely.translateBy(-100,-100);
+        //Print Square Not at Origin
+        //[(A, 110.0, 110.0); (B, 90.0, 110.0); (C, 90.0, 90.0); (D, 110.0, 90.0)]
+        //(Center of Square, 100.0, 100.0)
+
+
+        //To String Testing
+        System.out.println("toString Testing -> Should always be ABCD");
+        Shape lonely1 = lonely.translateBy(90,90);
+        System.out.println(lonely1);
+        Shape lonely2 = lonely1.translateBy(-15,15);
+        System.out.println(lonely2);
+        Shape lonely3 = lonely2.translateBy(100,-115);
+        System.out.println(lonely3);
+        System.out.println();
+        //toString Testing -> Should always be ABCD
+        //[(A, 100.0, 100.0); (B, 80.0, 100.0); (C, 80.0, 80.0); (D, 100.0, 80.0)]
+        //[(A, 85.0, 115.0); (B, 65.0, 115.0); (C, 65.0, 95.0); (D, 85.0, 95.0)]
+        //[(A, 185.0, 0.0); (B, 165.0, 0.0); (C, 165.0, -20.0); (D, 185.0, -20.0)]
+
+        //Translation Testing
+        Point a1 = new Point("A", 20, 20);
+        Point b1 = new Point("B", -20, 20);
+        Point c1 = new Point("C", -20, -20);
+        Point d1 = new Point("D", 20, -20);
+        Shape translator = new Square(a1,b1,c1,d1);
+        translator = translator.translateBy(-100,100);
+        System.out.println("\n" + "Translation Tests -> Should always be ABCD");
+        System.out.println("Original: " + translator);
+        translator = translator.translateBy(1000,-1000);
+        System.out.println("          " + translator);
+        translator = translator.translateBy(-1000,1000);
+        System.out.println("          " + translator);
+        System.out.println();
+        //Translation Tests -> Should always be ABCD
+        //Original: [(A, -80.0, 120.0); (B, -120.0, 120.0); (C, -120.0, 80.0); (D, -80.0, 80.0)]
+        //          [(A, 920.0, -880.0); (B, 880.0, -880.0); (C, 880.0, -920.0); (D, 920.0, -920.0)]
+        //          [(A, -80.0, 120.0); (B, -120.0, 120.0); (C, -120.0, 80.0); (D, -80.0, 80.0)]
 
         //Rotation Test
+        Shape rotator = new Square(a1,b1,c1,d1);
         System.out.println("\n" + "Rotate Tests");
-        System.out.println("Original: " + lonely);
-        lonely = lonely.rotateBy(90);
-        System.out.println("          " + lonely);
-        lonely = lonely.rotateBy(90);
-        System.out.println("          " + lonely);
-        lonely = lonely.rotateBy(90);
-        System.out.println("          " + lonely);
-        lonely = lonely.rotateBy(90);
-        System.out.println("          " + lonely);
+        System.out.println("Original: " + rotator);
+        rotator = rotator.rotateBy(90);
+        System.out.println("          " + rotator);
+        rotator = rotator.rotateBy(90);
+        System.out.println("          " + rotator);
+        rotator = rotator.rotateBy(90);
+        System.out.println("          " + rotator);
+        rotator = rotator.rotateBy(90);
+        System.out.println("          " + rotator);
+        System.out.println();
+        //Rotate Tests
+        //Original: [(A, 20.0, 20.0); (B, -20.0, 20.0); (C, -20.0, -20.0); (D, 20.0, -20.0)]
+        //          [(D, 20.0, 20.0); (A, -20.0, 20.0); (B, -20.0, -20.0); (C, 20.0, -20.0)]
+        //          [(C, 20.0, 20.0); (D, -20.0, 20.0); (A, -20.0, -20.0); (B, 20.0, -20.0)]
+        //          [(B, 20.0, 20.0); (C, -20.0, 20.0); (D, -20.0, -20.0); (A, 20.0, -20.0)]
+        //          [(A, 20.0, 20.0); (B, -20.0, 20.0); (C, -20.0, -20.0); (D, 20.0, -20.0)]
+
+        //Rotation Test 2
+        System.out.println("\n" + "Rotate Tests 2");
+        rotator = rotator.translateBy(50,-50);
+        System.out.println("Original: " + rotator);
+        rotator = rotator.rotateBy(90);
+        System.out.println("          " + rotator);
+        rotator = rotator.rotateBy(270);
+        System.out.println("          " + rotator);
+        rotator = rotator.rotateBy(-180);
+        System.out.println("          " + rotator);
+        rotator = rotator.rotateBy(-180);
+        System.out.println("          " + rotator);
+        System.out.println();
+        //Rotate Tests 2
+        //Original: [(A, 70.0, -30.0); (B, 30.0, -30.0); (C, 30.0, -70.0); (D, 70.0, -70.0)]
+        //          [(D, 70.0, -30.0); (A, 30.0, -30.0); (B, 30.0, -70.0); (C, 70.0, -70.0)]
+        //          [(A, 70.0, -30.0); (B, 30.0, -30.0); (C, 30.0, -70.0); (D, 70.0, -70.0)]
+        //          [(C, 70.0, -30.0); (D, 30.0, -30.0); (A, 30.0, -70.0); (B, 70.0, -70.0)]
+        //          [(A, 70.0, -30.0); (B, 30.0, -30.0); (C, 30.0, -70.0); (D, 70.0, -70.0)]
+
+        //Rotation Test 3
+        System.out.println("\n" + "Rotate Tests 3");
+        rotator = rotator.translateBy(10000,10000);
+        System.out.println("Original: " + rotator);
+        rotator = rotator.rotateBy(-90);
+        System.out.println("          " + rotator);
+        rotator = rotator.rotateBy(-90);
+        System.out.println("          " + rotator);
+        rotator = rotator.rotateBy(-90);
+        System.out.println("          " + rotator);
+        rotator = rotator.rotateBy(-90);
+        System.out.println("          " + rotator);
+        rotator = rotator.rotateBy(360);
+        System.out.println("          " + rotator);
+        //Rotate Tests 3
+        //Original: [(A, 10070.0, 9970.0); (B, 10030.0, 9970.0); (C, 10030.0, 9930.0); (D, 10070.0, 9930.0)]
+        //          [(B, 10070.0, 9970.0); (C, 10030.0, 9970.0); (D, 10030.0, 9930.0); (A, 10070.0, 9930.0)]
+        //          [(C, 10070.0, 9970.0); (D, 10030.0, 9970.0); (A, 10030.0, 9930.0); (B, 10070.0, 9930.0)]
+        //          [(D, 10070.0, 9970.0); (A, 10030.0, 9970.0); (B, 10030.0, 9930.0); (C, 10070.0, 9930.0)]
+        //          [(A, 10070.0, 9970.0); (B, 10030.0, 9970.0); (C, 10030.0, 9930.0); (D, 10070.0, 9930.0)]
+        //          [(A, 10070.0, 9970.0); (B, 10030.0, 9970.0); (C, 10030.0, 9930.0); (D, 10070.0, 9930.0)]
 
         System.out.println("\n" + "Illegal Argument Exceptions");
         Shape nope = new Square(north, toofarsouth, east, west);
